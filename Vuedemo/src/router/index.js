a04ab0823fcdc1e6a3ec881898ffe19020977e4d
anni369110 <594706774@qq.com>
2020-12-12 16:18:37 +0800

    测试

--- Vuedemo/src/router/index.js
+++ Vuedemo/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+const Index =() =>import('../views/index/index.vue')
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
+    // 重定向（登录）
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect:'/index',
+    },
+    {
+      path: '/index',
+      name: 'index',
+      component: Index
     }
   ]
 })
---
+++ Vuedemo/src/views/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="goods_detail_video" >
+
+     <div>{{msg}}</div>
+
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
+<script>
+
+export default {
+  name: "Login",
+  data() {
+    return {
+   msg:'测试测试',
+    };
+  },
+
+  methods: {
+
+  }
+};
+</script>
